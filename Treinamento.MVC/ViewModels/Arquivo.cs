989541d896978632650bc498e70064c9b58ac093
Wagner Mengue <dev.mengue@gmail.com>
2016-02-08 11:02:58 -0200

    Criado primeiro viewModel

---
+++ Treinamento.MVC/ViewModels/Arquivo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Treinamento.MVC.ViewModels
+{
+    public class Arquivo
+    {
+        [Key]
+        public int IdArquivo { get; set; }
+
+        public int IdProjeto { get; set; }
+        //public virtual Projeto Projeto { get; set; }
+
+        [Required(ErrorMessage = "Preencha o nome")]
+        [MaxLength(150, ErrorMessage = "Tamanho máximo para o nome é de {0} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o caminho")]
+        public string Caminho { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int IdUsuario { get; set; }
+        //public virtual Usuario Usuario { get; set; }
+    }
+}
