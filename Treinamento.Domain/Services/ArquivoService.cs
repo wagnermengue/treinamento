43705e85ebf3e937f2938a75f405cc31aecbd5f1
Wagner Mengue <dev.mengue@gmail.com>
2016-02-09 08:56:41 -0200

    Criado interface de servico e servico base.

---
+++ Treinamento.Domain/Services/ArquivoService.cs
@@ -0,0 +1,17 @@
+﻿using Treinamento.Domain.Entities;
+using Treinamento.Domain.Interfaces.Repositories;
+using Treinamento.Domain.Interfaces.Services;
+
+namespace Treinamento.Domain.Services
+{
+    public class ArquivoService : ServiceBase<Arquivo>, IArquivoService
+    {
+        private readonly IArquivoRepository _arquivoRepository;
+
+        public ArquivoService(IArquivoRepository arquivoRepository)
+            :base(arquivoRepository)
+        {
+            _arquivoRepository = arquivoRepository;
+        }
+    }
+}
