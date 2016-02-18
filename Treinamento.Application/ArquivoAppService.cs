ea1a94ecb7b485825b0a305d238a2989229aa894
Wagner Mengue <dev.mengue@gmail.com>
2016-02-18 13:12:52 -0200

    Criado Aplicação base e Interface base. Necessário implementar as entidades específicas.

---
+++ Treinamento.Application/ArquivoAppService.cs
@@ -0,0 +1,17 @@
+﻿using Treinamento.Application.Interfaces;
+using Treinamento.Domain.Entities;
+using Treinamento.Domain.Interfaces.Services;
+
+namespace Treinamento.Application
+{
+    public class ArquivoAppService : AppServiceBase<Arquivo>, IArquivoAppService
+    {
+        private readonly IArquivoService _arquivoService;
+
+        public ArquivoAppService(IArquivoService arquivoService)
+            :base(arquivoService)
+        {
+            _arquivoService = arquivoService;
+        }
+    }
+}
